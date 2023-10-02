07a16dea2b5dd31278722989fa617711162ee190
Alberto Barrago <alberto.barrago@softfobia.com>
2023-10-02 23:35:03 +0200

    Init commit

---
+++ src/main/java/com/love/java21/LoveJava21Application.java
@@ -0,0 +1,13 @@
+package com.love.java21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveJava21Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoveJava21Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/love/java21/LoveJava21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.love.java21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoveJava21ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
